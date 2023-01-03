12c35afe58e81db63aaee466112ee7542d39c77a
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-01-03 10:34:53 +0100

    Logo added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Login from "./scenes/auth/Login";
-
 import SideBar from "./scenes/global/Sidebar";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
@@ -33,7 +32,7 @@ function App() {
             <Topbar />
             <Routes>
               {/* <Route path="/" element={<Login />} /> */}
-              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+              <Route path="/dashboard" element={<Dashboard />} />
               {/* <Route path="/team" element={<Team />} /> */}
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
